c6316a18e1a915568db6357fd01915c3e2ec879d
William Rodrigues <99099977+oWilliamRodrigues@users.noreply.github.com>
2024-05-15 17:36:54 -0300

    First Commit
    Adicionando os códigos já feitos até o momento

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'HTML Body',
+        productNumber: '12341',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Style',
+        productNumber: '56788',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'JS Script',
+        productNumber: '91011',
+        paymentStatus : 'Paid',
+        status: 'Active'
+    }
+]
